2034f0c86a220ff54c3c9a1d7fd72b03b2664fac
Omid Jafari <mbwolf20@gmail.com>
2023-11-12 20:19:29 +0330

    feat: :art: added container for modal

---
+++ app/components/common/modalContainer.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+const ModalContainer = ({
+  open,
+  children,
+}: {
+  open: boolean;
+  children: React.ReactNode;
+}) => {
+  return (
+    <div
+      className={`absolute top-0 left-0 w-full h-[100vh] z-20 px-2 overflow-hidden transition-all
+
+      duration-700 ${
+        open ? "bg-[#57959ba7] max-h-[10000px]" : "bg-[#57959b00] max-h-0"
+      }`}
+    >
+      <div
+        className={`bg-[url(/modalBG.png)] bg-contain w-full h-[100vh] bg-no-repeat animate__animated ${
+          open ? "animate__fadeInDownBig" : "animate__fadeOutUpBig"
+        }`}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default ModalContainer;
