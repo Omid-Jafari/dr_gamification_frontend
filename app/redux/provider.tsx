603e4051e8f78fb12a400837f29b346e28be7110
Omid Jafari <mbwolf20@gmail.com>
2023-11-15 09:22:51 +0330

    feat: :art: added porovider for react query and axios

--- app/redux/provider.tsx
+++ app/redux/provider.tsx
@@ -1,8 +1,14 @@
 "use client";
 
+import { QueryClient, QueryClientProvider } from "react-query";
 import { store } from "./store";
 import { Provider } from "react-redux";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
+  const client = new QueryClient();
+  return (
+    <Provider store={store}>
+      <QueryClientProvider client={client}>{children}</QueryClientProvider>
+    </Provider>
+  );
 }
