d7167361ace602e726a466f207d2e489a4a34891
Omid Jafari <mbwolf20@gmail.com>
2023-11-15 13:12:05 +0330

    feat: added header

--- app/header.tsx
+++ app/header.tsx
@@ -1,5 +1,27 @@
+"use client";
+
+// import { RootState } from "@/app/redux/store";
+import { useSelector } from "react-redux";
+import { RootState } from "./redux/store";
+import { useRouter } from "next/navigation";
+
 const Header = () => {
-  return <div className="w-full h-[120px]">asdafasdf</div>;
+  const { user } = useSelector((state: RootState) => state.user);
+  const router = useRouter();
+
+  return (
+    <div className="w-full h-[120px] flex items-center justify-between px-5">
+      <button onClick={() => router.back()}>
+        <img src="/Back.svg" alt="" />
+      </button>
+      <button onClick={() => {}}>
+        <img src="/Sound.svg" alt="" />
+      </button>
+      <button onClick={() => {}}>
+        <img src="/Group.svg" alt="" />
+      </button>
+    </div>
+  );
 };
 
 export default Header;
