d9678ccea4b246cfd96be53326ec1c1220112ec3
Omid Jafari <mbwolf20@gmail.com>
2023-11-18 00:52:10 +0330

    docs: :sparkles: added update user route

--- app/api/ApiClient.ts
+++ app/api/ApiClient.ts
@@ -19,3 +19,6 @@ export const userData = (id: string | null) => {
 export const signinUser = (body: object) => {
   return useAxios.post(`/signin`, body);
 };
+export const updateUser = ({ _id, body }: { _id: number; body: object }) => {
+  return useAxios.patch(`/user/${_id}`, body);
+};
