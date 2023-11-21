1ec338daab23858a9bd97418cbb48ebd3cdf808a
Omid Jafari <mbwolf20@gmail.com>
2023-11-21 09:48:53 +0330

    feat: :art: added bg for records page

--- app/records/page.tsx
+++ app/records/page.tsx
@@ -29,10 +29,21 @@ const Records = () => {
 
   return (
     <div
-      className={`absolute top-0 left-0 w-full h-[100vh] z-20 px-2 overflow-hidden transition-all duration-[1s] ${
-        open ? "bg-[#57959ba7] max-h-[10000px]" : "bg-[#57959b00] max-h-0"
+      className={`absolute top-0 left-0 w-full h-[100vh] z-20 px-2 overflow-hidden transition-all duration-[.7s] ${
+        open
+          ? "bg-[#57959ba7] max-h-[10000px] duration-[.7s]"
+          : "bg-[#57959b00] max-h-0 duration-[1.7s]"
       }`}
     >
+      <img
+        src="/ModalBlueBg.svg"
+        className={`absolute top-0 left-0 w-full h-full animate__animated animate__delay ${
+          open
+            ? "animate__fadeIn animate__slow"
+            : "animate__fadeOut animate__fast"
+        }`}
+        alt=""
+      />
       <div
         className={`w-full h-[100vh] flex flex-col items-center justify-start pt-6 gap-4 animate__animated ${
           open ? "animate__fadeInDownBig" : "animate__fadeOutUpBig"
