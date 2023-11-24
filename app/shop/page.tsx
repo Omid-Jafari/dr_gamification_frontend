1de995c3b9de49b4830f39bb86a76f9389242aa9
Omid Jafari <mbwolf20@gmail.com>
2023-11-24 08:54:41 +0330

    feat: updated shop page

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -3,9 +3,12 @@
 import { useRef } from "react";
 import QuestionModal from "../components/common/questionModal";
 import {
+  fifthQuest,
   forthQuest,
   fristQuest,
   secondQuest,
+  seventhQuest,
+  sixthQuest,
   thirdQuest,
 } from "../constant/questions/shop";
 import { useSelector } from "react-redux";
@@ -23,6 +26,9 @@ const Shop = () => {
   const secondQuestRef: any = useRef(null);
   const thirdQuestRef: any = useRef(null);
   const forthQuestRef: any = useRef(null);
+  const fifthQuestRef: any = useRef(null);
+  const sixthQuestRef: any = useRef(null);
+  const seventhQuestRef: any = useRef(null);
   const congratsRef: any = useRef(null);
   const congratsRefOpen = (data: {}) => {
     congratsRef.current && congratsRef.current.openModal(data);
@@ -48,6 +54,15 @@ const Shop = () => {
   const openForthQuestRef = () => {
     forthQuestRef.current && forthQuestRef.current.openModal();
   };
+  const openFifthQuestRef = () => {
+    fifthQuestRef.current && fifthQuestRef.current.openModal();
+  };
+  const openSixthQuestRef = () => {
+    sixthQuestRef.current && sixthQuestRef.current.openModal();
+  };
+  const openSeventhQuestRef = () => {
+    seventhQuestRef.current && seventhQuestRef.current.openModal();
+  };
   const { user } = useSelector((state: RootState) => state.user);
 
   const checkAnswered = (questParam: string) => {
@@ -67,11 +82,11 @@ const Shop = () => {
       <ConfirmModal ref={confirmRef} openWheelRef={openWheelRef} />
       <GuideModal ref={guideRef} />
       <WheelSpinnerModal congratsRefOpen={congratsRefOpen} ref={wheelRef} />
-      {checkAnswered("transFirstQuest") && (
+      {checkAnswered("shopFirstQuest") && (
         <>
           <QuestionModal
             question={fristQuest}
-            whichQuest={"transFirstQuest"}
+            whichQuest={"shopFirstQuest"}
             openConfirmRef={openConfirmRef}
             openGuideRef={openGuideRef}
             ref={fristQuestRef}
@@ -79,21 +94,21 @@ const Shop = () => {
           <button
             type="button"
             onClick={openFristQuestRef}
-            className="absolute bottom-[3%] w-[20%] left-[56%]"
+            className="absolute bottom-[7%] w-[20%] left-[53%]"
           >
             <img
               src="/homeIcons/shop.png"
-              alt="transportation icon"
+              alt="shop icon"
               className="w-full h-auto animate-prjj"
             />
           </button>
         </>
       )}
-      {checkAnswered("transSecondQuest") && (
+      {checkAnswered("shopSecondQuest") && (
         <>
           <QuestionModal
             question={secondQuest}
-            whichQuest={"transSecondQuest"}
+            whichQuest={"shopSecondQuest"}
             openConfirmRef={openConfirmRef}
             openGuideRef={openGuideRef}
             ref={secondQuestRef}
@@ -101,21 +116,21 @@ const Shop = () => {
           <button
             type="button"
             onClick={openSecondQuestRef}
-            className="absolute top-[56%] w-[20%] left-[3%]"
+            className="absolute bottom-[8%] w-[20%] right-[15%]"
           >
             <img
               src="/homeIcons/shop.png"
-              alt="transportation icon"
+              alt="shop icon"
               className="w-full h-auto animate-prjj"
             />
           </button>
         </>
       )}
-      {checkAnswered("transThirdQuest") && (
+      {checkAnswered("shopThirdQuest") && (
         <>
           <QuestionModal
             question={thirdQuest}
-            whichQuest={"transThirdQuest"}
+            whichQuest={"shopThirdQuest"}
             openConfirmRef={openConfirmRef}
             openGuideRef={openGuideRef}
             ref={thirdQuestRef}
@@ -123,21 +138,21 @@ const Shop = () => {
           <button
             type="button"
             onClick={openThirdQuestRef}
-            className="absolute bottom-[11%] w-[20%] left-[44%]"
+            className="absolute bottom-[11%] w-[20%] left-[14%]"
           >
             <img
               src="/homeIcons/shop.png"
-              alt="transportation icon"
+              alt="shop icon"
               className="w-full h-auto animate-prjj"
             />
           </button>
         </>
       )}
-      {checkAnswered("transForthQuest") && (
+      {checkAnswered("shopForthQuest") && (
         <>
           <QuestionModal
             question={forthQuest}
-            whichQuest={"transForthQuest"}
+            whichQuest={"shopForthQuest"}
             openConfirmRef={openConfirmRef}
             openGuideRef={openGuideRef}
             ref={forthQuestRef}
@@ -145,11 +160,77 @@ const Shop = () => {
           <button
             type="button"
             onClick={openForthQuestRef}
-            className="absolute top-[56%] w-[20%] left-[30%]"
+            className="absolute bottom-[10%] w-[20%] left-[37%]"
           >
             <img
               src="/homeIcons/shop.png"
-              alt="transportation icon"
+              alt="shop icon"
+              className="w-full h-auto animate-prjj"
+            />
+          </button>
+        </>
+      )}
+      {checkAnswered("shopFifthQuest") && (
+        <>
+          <QuestionModal
+            question={fifthQuest}
+            whichQuest={"shopFifthQuest"}
+            openConfirmRef={openConfirmRef}
+            openGuideRef={openGuideRef}
+            ref={fifthQuestRef}
+          />
+          <button
+            type="button"
+            onClick={openFifthQuestRef}
+            className="absolute bottom-[17%] w-[20%] left-[44%]"
+          >
+            <img
+              src="/homeIcons/shop.png"
+              alt="shop icon"
+              className="w-full h-auto animate-prjj"
+            />
+          </button>
+        </>
+      )}
+      {checkAnswered("shopSixthQuest") && (
+        <>
+          <QuestionModal
+            question={sixthQuest}
+            whichQuest={"shopSixthQuest"}
+            openConfirmRef={openConfirmRef}
+            openGuideRef={openGuideRef}
+            ref={sixthQuestRef}
+          />
+          <button
+            type="button"
+            onClick={openSixthQuestRef}
+            className="absolute bottom-[18%] w-[20%] right-[2%]"
+          >
+            <img
+              src="/homeIcons/shop.png"
+              alt="shop icon"
+              className="w-full h-auto animate-prjj"
+            />
+          </button>
+        </>
+      )}
+      {checkAnswered("shopSeventhQuest") && (
+        <>
+          <QuestionModal
+            question={seventhQuest}
+            whichQuest={"shopSeventhQuest"}
+            openConfirmRef={openConfirmRef}
+            openGuideRef={openGuideRef}
+            ref={seventhQuestRef}
+          />
+          <button
+            type="button"
+            onClick={openSeventhQuestRef}
+            className="absolute bottom-[20%] w-[20%] left-[26%]"
+          >
+            <img
+              src="/homeIcons/shop.png"
+              alt="shop icon"
               className="w-full h-auto animate-prjj"
             />
           </button>
